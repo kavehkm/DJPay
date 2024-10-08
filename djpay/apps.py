bd7a0cf71acf599d4ba8d481de21a4f1a5bec063
kavehkm <mehrbaniankaveh@gmail.com>
2024-10-08 16:13:41 +0330

    fix type: rename djpy -> djpay and also remove redundant modules for current stage of app

--- djpay/apps.py
+++ djpay/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DjpyConfig(AppConfig):
+class DjpayConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "djpy"
+    name = "djpay"

e7b34e0f79cf8eb6f6182eddbeeaa1c35ff73ed0
kavehkm <mehrbaniankaveh@gmail.com>
2024-10-11 19:20:39 +0330

    update utils.py: 1) add is_serializable() function to check given value can be serialized as json part or not 2) add clean_json_data() function to clean given data from unserializable value recursively

--- djpay/utils.py
+++ djpay/utils.py
@@ -1,3 +1,7 @@
+# standard
+import json
+from typing import Any
+
 # dj
 from django.urls import reverse
 from django.http.request import HttpRequest
@@ -12,3 +16,20 @@ def absolute_reverse(
     return request.build_absolute_uri(reverse(view, args=args, kwargs=kwargs)).replace(
         "127.0.0.1", "localhost"
     )
+
+
+def is_serializable(value: Any) -> Any:
+    try:
+        json.dumps(value)
+        return True
+    except (TypeError, ValueError):
+        return False
+
+
+def clean_json_data(data: Any) -> Any:
+    if isinstance(data, dict):
+        return {k: clean_json_data(v) for k, v in data.items() if is_serializable(v)}
+    elif isinstance(data, list):
+        return [clean_json_data(item) for item in data if is_serializable(item)]
+    else:
+        return data if is_serializable(data) else None

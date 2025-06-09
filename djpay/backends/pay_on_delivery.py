aa52eaa5aaa544ae308bbb0a2b28f7a029bda966
kavehkm <mehrbaniankaveh@gmail.com>
2025-06-09 14:22:37 +0330

    update PayOnDelivery class: 1) replace raw raise PaymentError() with self.error()

--- djpay/backends/pay_on_delivery.py
+++ djpay/backends/pay_on_delivery.py
@@ -4,7 +4,6 @@ from typing import Any
 # internal
 from ..models import Bill
 from .base import BaseBackend
-from ..errors import PaymentError
 
 
 class PayOnDelivery(BaseBackend):
@@ -19,10 +18,10 @@ class PayOnDelivery(BaseBackend):
     def verify(self, bill: Bill, **kwargs: Any) -> Bill:
         # check for backend
         if bill.backend != self.identifier:
-            raise PaymentError("Invalid bill.")
+            self.error("Invalid bill.")
         # check verified status
         if bill.verified:
-            raise PaymentError("Invalid bill.")
+            self.error("Invalid bill.")
         # verify and return bill
         bill.verified = True
         bill.save(update_fields=["verified"])
